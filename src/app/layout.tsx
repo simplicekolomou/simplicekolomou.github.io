b13d75950738a37af1e85f88e91247bd4a9863d3
simplicekolomou <s.kolomou@student.helmo.be>
2026-07-01 03:38:27 +0200

    configuration de google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ThemeProvider from "@/components/theme/ThemeProvider";
 import Footer from "@/components/footer/Footer";
 import type {Metadata} from "next";
 import ClarityProvider from "@/components/analytics/Clarity";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "Simplice Kolomou",
@@ -16,6 +17,7 @@ export const metadata: Metadata = {
     },
 };
 export default function RootLayout({children}: Readonly<{ children: ReactNode }>) {
+    const gaId = process.env.NEXT_PUBLIC_GA_ID;
     return (
         <html lang="fr" suppressHydrationWarning>
         <body className="min-h-screen flex flex-col">
@@ -24,6 +26,23 @@ export default function RootLayout({children}: Readonly<{ children: ReactNode }>
             <NavBar/>
             <main className="flex-1">
                 {children}
+                {gaId && (
+                    <>
+                        <Script
+                            strategy="afterInteractive"
+                            src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+                        />
+
+                        <Script id="ga-init" strategy="afterInteractive">
+                            {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${gaId}');
+                          `}
+                        </Script>
+                    </>
+                )}
             </main>
             <Footer/>
         </ThemeProvider>
