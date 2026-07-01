3a64e69f818675f8a9552936c2256f7582eb53f4
simplicekolomou <s.kolomou@student.helmo.be>
2026-07-01 02:56:41 +0200

    configuration de clarity

--- src/components/analytics/Clarity.tsx
+++ src/components/analytics/Clarity.tsx
@@ -4,6 +4,9 @@ import Clarity from '@microsoft/clarity';
 
 export default function ClarityProvider() {
     useEffect(() => {
+        if (process.env.NODE_ENV !== "production") {
+            return;
+        }
         const projectId = process.env.NEXT_PUBLIC_CLARITY_ID;
         if(!projectId) return
         Clarity.init(projectId);
