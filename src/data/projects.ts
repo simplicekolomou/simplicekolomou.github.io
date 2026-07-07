498935841458e1e8edcc8c680f9d371017c41dd2
simplicekolomou <s.kolomou@student.helmo.be>
2026-07-08 00:23:10 +0200

    adaption du lien de la démo de bookexchange

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -143,7 +143,7 @@ const myProjects: Project[] = [
             "https://github.com/simplicekolomou/bookexchange-front",
             "https://github.com/simplicekolomou/bookexchange-api"
         ],
-        demo: "https://bookexchange-front.vercel.app/",
+        demo: "https://bookexchange-snowy.vercel.app/",
         type: "team",
         team: ["Mathieu", "Théo"],
         image: "/images/bookexchange/bookexchange-presentation.png",
